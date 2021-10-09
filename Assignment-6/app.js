0d7121d611181870132e7cf163116c18a068d64f
kratijain-9 <77618906+kratijain-9@users.noreply.github.com>
2021-10-09 22:27:03 +0530

    Update app.js

--- Assignment-6/app.js
+++ Assignment-6/app.js
@@ -1,26 +1,48 @@
 function add() { // to add tasks to the list
     let i = document.querySelector('input');
-    let text = i.value;
-    //create li
+    let text = " "+i.value;
+    if(i.value!=" "){
+        //create li
     let li = document.createElement('li');
-    li.innerHTML = text;
-
+    li.innerHTML = `<span><i class="fas fa-trash-alt"></i></span>${text}`;
+    
     let ul = document.querySelector('ul');
     ul.appendChild(li);
-    li.addEventListener('click', () => {
-        li.remove();
+    let sc=li.children;
+    
+    sc[0].addEventListener('click', () => {
+        // sc[0].remove();
+        sc[0].parentElement.remove();
+    })
+    li.addEventListener('click',()=>{
+        li.style.textDecoration='line-through';
     })
     //clear input box
     i.value = " ";
+
+    }
+    
 }
 
 
 let a = document.querySelector('.btn');
 a.addEventListener('click', add);
 
-let li = document.querySelectorAll('li');
-for (let k of li) {
+let sp = document.querySelectorAll('span');
+for (let k of sp) {
     k.addEventListener('click', () => {
-        k.remove();
+        
+        let pe=k.parentElement;
+        pe.remove();
+        
+    })
+    
+}
+
+let li=document.querySelectorAll('li');
+for(let k of li){
+    k.addEventListener('click',()=>{
+        k.style.textDecoration='line-through';
+       
     })
 }
