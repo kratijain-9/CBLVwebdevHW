fa473a58f5c4b409d31d39de1b2c3b970f492e1c
kratijain-9 <77618906+kratijain-9@users.noreply.github.com>
2021-10-15 21:56:20 +0530

    Update app.js
    revolve effect

--- Assignment-7/app.js
+++ Assignment-7/app.js
@@ -1,12 +1,12 @@
 const days = {
 
-    "0": "Monday",
-    "1": "Tuesday",
-    "2": "Wednesday",
-    "3": "Thursday",
-    "4": "Friday",
-    "5": "Saturday",
-    "6": "Sunday"
+    "1": "Monday",
+    "2": "Tuesday",
+    "3": "Wednesday",
+    "4": "Thursday",
+    "5": "Friday",
+    "6": "Saturday",
+    "0": "Sunday"
 
 }
 
@@ -68,7 +68,10 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     // console.log('pressed');
     const city = inp.value;
-    revolve.setAttribute('class', 'nc');
+    revolve.classList.toggle('nc');
+    setTimeout(()=>{
+        revolve.classList.toggle('nc');
+    },700)
     getweather(city);
 
 })
